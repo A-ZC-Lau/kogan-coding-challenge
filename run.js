8f9520d1c4e3051da0d68ef8b87f4c3956b517e4
Alexander Lau <alexanderlau@Alexanders-MacBook-Pro.local>
2018-09-11 19:55:45 +1000

    [run.js] working

--- run.js
+++ run.js
@@ -1,3 +1,53 @@
 const axios = require("axios")
+const {
+    calculateCubicWeight,
+    createUrl,
+    getData,
+} = require("./functions.js")
 
-const url = "http://wp8m3he1wt.s3-website-ap-southeast-2.amazonaws.com/api/products/1"
+run()
+
+async function run()
+{
+    const data = []
+    
+    let path = "/api/products/1"
+    let url = createUrl(path)
+    do {
+        const category = "Air Conditioners"
+        let response, result
+        try 
+        {
+            response = await getData(url)
+            result = response.data
+        } 
+        catch (error) 
+        {
+            console.error(error)    
+        }
+        
+        let filtered_results = result.objects.filter( element => element.category === category)
+        for (let element of filtered_results)
+        {
+            let all_sizes = Object.values(element.size)
+            if (all_sizes.length !== 3)
+            {
+                throw Error("there are more sizes than expected")
+            }
+
+            all_sizes = all_sizes.map( size => size / 100 ) // convert to meters
+            element["average cubic weight"] = calculateCubicWeight(...all_sizes)
+        }
+
+        data.push(...filtered_results)
+        if (!result.next)
+        {
+            break
+        }
+        url = createUrl(result.next)
+    }
+    while (url)
+
+    console.log(JSON.stringify(data, null, 4))
+    return data
+}
